90acd50a76d40dd822b00e056ae83353d276cb04
Claudio Klingler <ck@realtime-projects.com>
2011-06-23 15:05:57 +0200

    Added documentation to test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+### (((1 mksource automated test
+#
+# This script automatically tests the mksource projects.
+#
+# Test output files will be written to the test/output directory. test.sh creates this directory, if it does not
+# exists. The tests check correct functionality of mksource.
+###
+
 mksource="$(pwd)/mksource";
 
+### {{{1 testing tools
+
+### {{{2 function fail - abort the script with an error message
 function fail
 {
     echo "ABORTING: $1";
     exit 1;
 }
 
+### {{{2 function assert - check a condition and fail if it is not fulfilled
 function assert
 {
     echo -n "checking $1..."
@@ -15,11 +27,15 @@ function assert
     echo "OK";
 }
 
+### {{{1 test helper functions
+
+### {{{2 function cleanup - cleanup test environment
 function cleanup
 {
     rm -f test/output/TestCreatePerlFile.pl;
 }
 
+### {{{2 function startup - prepare the tests
 function startup
 {
     if [ ! -e test/output ]; then mkdir -p test/output; fi;
